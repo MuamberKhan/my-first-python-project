09fe41bf2e5734741618cfa95a70403fb6e6d0e4
Muamber Ali <muamberalikhan@gmail.com>
2020-04-26 23:22:17 +0500

    initial commit after configuration provided

---
+++ script.py
@@ -0,0 +1,21 @@
+import requests
+'''
+import math
+import os
+import sys as system
+
+print(system.version)
+print(system.executable)
+
+def Great(who_to_great):
+    greeting = "hello,{}".format(who_to_great)
+    return greeting
+
+# print(Great("World"))
+# print(Great("Muamber"))
+r = requests.get("https://www.facebook.com")
+print(r)
+'''
+
+name=input("Please enter your name! ")
+print("Hello,",name)
